76265a697ec5df4ace3516b8fbd75d5bb215cf53
ajpcu <akanksha.jharia23@pcu.edu.in>
2025-02-10 12:11:25 +0530

    update 1 - contacts

--- version/app.js
+++ version/app.js
@@ -5,10 +5,9 @@ var response;
  
 app.get('/', function (req, res) {
     response = 'This is version 1 of the app.' + '\n';
-    
+
     //send the response to the client
     res.send(response);
-
 });
 
 app.listen(8080, function () {
