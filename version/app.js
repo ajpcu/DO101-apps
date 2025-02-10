64bc16f0ac275c224ba00b84dfc3051178bb7064
ajpcu <akanksha.jharia23@pcu.edu.in>
2025-02-10 12:23:36 +0530

    update 1 - devenv

--- version/app.js
+++ version/app.js
@@ -8,7 +8,6 @@ app.get('/', function (req, res) {
     response = 'This is version 1 of the app.' + '\n';
     //send the response to the client
     res.send(response);
-
 });
 
 app.listen(8080, function () {
